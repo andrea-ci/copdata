2913be13eadb714797598698f901b4dcfb1875cf
andrea <andrea.capitanelli@gmail.com>
2022-03-01 10:30:33 +0100

    added examples

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # Processing Sentinel-1 radar images
 
 Minimal code for classification and object detection tasks.
+
+# Quickstart
+
+Init the engine:
+```python
+from core import SatEngine
+
+# Init the list of downloaded products.
+engine = SatEngine('username', 'password',
+    dir_downloads = '/path/to/downloads',
+    dir_products = '/path/to/data',
+    dir_images = '/path/to/images')
+```
+
+Load metadata from CSV:
+```python
+engine.from_csv('path/to/metadata.csv')
+```
+
+Query Copernicus and download products:
+```python
+fn_geojson = 'aoi.geojson'
+date_start = '20210101'
+date_end = '20211231'
+
+engine.download(fn_geojson, date_start, date_end, policy = 'Contains',
+  check_only = False, query = False)
+```
+
+Extract AOI and detect ships:
+```python
+engine.extract_aoi(fn_geojson)
+res = engine.detect_ships()
+```
+
+Save metadata (including detection results) to CSV:
+```python
+engine.to_csv('path/to/metadata.csv')
+```
--- example.py
+++ example.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 """This script perform a query to Copernicus, filters and downloads valid products
 and extracts to their SAFE folders. """
-from os import path
-import zipfile
-import geoutils as gt
-from products import ProductStore
-from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
-from config import PROD_LIST, DIR_DOWNLOADS, DIR_PRODUCTS, DIR_GEO
-from core import ProductStore, SatEngine
+from core import SatEngine
 
 fn_geojson = 'geoareas/naples.geojson'
-date_start = '20220210'
-date_end = '20220224'
+date_start = '20170101'
+date_end = '20211231'
 
 # Init the list of downloaded products.
-engine = SatEngine('andrea32874763', 'yigXUR6M!eunDdv')
+engine = SatEngine('andrea32874763', 'yigXUR6M!eunDdv',
+    dir_downloads = '/hdd2/sentinel-products/downloads',
+    dir_products = '/hdd2/sentinel-products/data',
+    dir_images = '/hdd2/sentinel-products/images')
 
-#engine.download(fn_geojson, date_start, date_end, policy = 'Contains')
-#engine.extract_aoi(fn_geojson)
+engine.from_csv('napoli-2017-2021.csv')
+engine.download(fn_geojson, date_start, date_end, policy = 'Contains',
+    check_only = False, query = False)
+#engine.to_csv('napoli-2017-2021.csv')
+exit()
 
-engine.from_csv('prod_metadata.csv')
+engine.extract_aoi(fn_geojson)
 res = engine.detect_ships()
-engine.to_csv('prod_metadata.csv')
